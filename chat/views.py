e5a0d92e6f1db5e046fd5512afcb8023c41bbb1e
veryacademy <35196453+veryacademy@users.noreply.github.com>
2020-09-01 22:04:51 +0100

    nc

---
+++ chat/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def room(request, room_name):
+    return render(request, 'chatroom.html', {
+        'room_name': room_name
+    })
